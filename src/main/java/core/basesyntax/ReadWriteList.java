f76bc018a92190a31b6528eab88bd08f77bfd5c9
Nazar <258258>
2022-08-29 20:43:44 +0300

    refactored after review

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,29 +11,32 @@ public class ReadWriteList<E> {
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        lock.writeLock().lock();
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
         try {
             list.add(element);
         } finally {
-            lock.writeLock().unlock();
+            writeLock.unlock();
         }
     }
 
     public E get(int index) {
-        lock.readLock().lock();
+        Lock readLock = lock.readLock();
+        readLock.lock();
         try {
             return list.get(index);
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
     }
 
     public int size() {
-        lock.readLock().lock();
+        Lock readLock = lock.readLock();
+        readLock.lock();
         try {
             return list.size();
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
     }
 }
