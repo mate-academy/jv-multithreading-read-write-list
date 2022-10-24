9f1032439ec8ec9ca7c2a1dcb2e67ef761a32357
Nazar <litvik.12@gmail.com>
2022-10-24 12:38:46 +0300

    added right lock methods

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -21,22 +21,22 @@ public class ReadWriteList<E> {
     }
 
     public E get(int index) {
-        Lock writeLock = lock.writeLock();
-        writeLock.lock();
+        Lock readLock = lock.readLock();
+        readLock.lock();
         try {
             return list.get(index);
         } finally {
-            writeLock.unlock();
+            readLock.unlock();
         }
     }
 
     public int size() {
-        Lock writeLock = lock.writeLock();
-        writeLock.lock();
+        Lock readLock = lock.readLock();
+        readLock.lock();
         try {
             return list.size();
         } finally {
-            writeLock.unlock();
+            readLock.unlock();
         }
     }
 }
