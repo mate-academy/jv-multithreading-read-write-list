aa21116433c13653fa293db596107c751e7b06d1
X3NON-135 <immort4ld3momo@gmail.com>
2022-11-23 09:15:24 +0000

    implemented methods to allow multi threads to  read data at the same time

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,24 +2,41 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
     private List<E> list = new ArrayList<>();
     private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private Lock writeLock = lock.writeLock();
 
     public void add(E element) {
-        // write your code here
+        writeLock.lock();
+        try {
+            list.add(element);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            return list.get(index);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            return list.size();
+        } finally {
+            writeLock.unlock();
+        }
     }
 }
