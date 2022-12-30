056ca39fe8d27ec46d0fd5b17b4650df85d1d277
Andrew Miroshnikov <giggle2594@gmail.com>
2022-12-30 13:48:04 +0200

    Task completed

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,16 +11,32 @@ public class ReadWriteList<E> {
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        // write your code here
+        Lock lockWrite = lock.writeLock();
+        lockWrite.lock();
+        try {
+            list.add(element);
+        } finally {
+            lockWrite.unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        Lock lockRead = lock.readLock();
+        lockRead.lock();
+        try {
+            return list.get(index);
+        } finally {
+            lockRead.unlock();
+        }
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        Lock lockRead = lock.readLock();
+        lockRead.lock();
+        try {
+            return list.size();
+        } finally {
+            lockRead.unlock();
+        }
     }
 }
