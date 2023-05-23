87e82e8c243095e962ba1722796ceaabcaa8c4c7
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-05-23 11:40:56 +0300

    did task "Read-write list"

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,16 +11,37 @@ public class ReadWriteList<E> {
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        // write your code here
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+
+        try {
+            list.add(element);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        E object = null;
+        try {
+            object = list.get(index);
+        } finally {
+            readLock.unlock();
+        }
+        return object;
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        Lock writelock = lock.writeLock();
+        writelock.lock();
+        int size = 0;
+        try {
+            size = list.size();
+        } finally {
+            writelock.unlock();
+        }
+        return size;
     }
 }
