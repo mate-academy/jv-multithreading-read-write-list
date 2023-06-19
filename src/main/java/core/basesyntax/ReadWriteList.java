c6094ff22361f88d8373a87448e02431af64ec3c
WallRun <dimonty2003@gmail.com>
2023-06-19 10:57:32 +0300

    Removed redundant size variable

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,20 +2,17 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
     private final List<E> list = new ArrayList<>();
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final AtomicInteger size = new AtomicInteger();
 
     public void add(E element) {
         try {
             lock.writeLock().lock();
             list.add(element);
-            size.getAndIncrement();
         } finally {
             lock.writeLock().unlock();
         }
@@ -31,6 +28,6 @@ public class ReadWriteList<E> {
     }
 
     public int size() {
-        return size.get();
+        return list.size();
     }
 }
