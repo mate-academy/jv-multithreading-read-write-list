e2012404f7f272c8fa7db60901635cbbe777fda8
WallRun <dimonty2003@gmail.com>
2023-06-19 13:39:05 +0300

    Added read lock to size method

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -28,6 +28,11 @@ public class ReadWriteList<E> {
     }
 
     public int size() {
-        return list.size();
+        try {
+            lock.readLock().lock();
+            return list.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
