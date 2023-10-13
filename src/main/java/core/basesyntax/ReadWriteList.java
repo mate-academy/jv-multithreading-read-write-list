430301e69e54f3701f6aa121ee30c6360ac7cb0d
Visqe <veetee@tutanota.com>
2023-10-13 12:48:25 +0300

    added solution

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,24 +2,45 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
-    private List<E> list = new ArrayList<>();
-    private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final List<E> list = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        // write your code here
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            list.add(element);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        E element;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            element = list.get(index);
+        } finally {
+            readLock.unlock();
+        }
+        return element;
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        int size;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            size = list.size();
+        } finally {
+            readLock.unlock();
+        }
+        return size;
     }
 }
