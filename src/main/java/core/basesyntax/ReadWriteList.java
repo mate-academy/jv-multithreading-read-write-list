241b089969bac314471dec1d3c2955ad6966248b
Igor Korolevich <igorkorolj@gmail.com>
2024-01-23 22:35:37 +0200

    added lock

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,16 +11,29 @@ public class ReadWriteList<E> {
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
+        Lock wLock = lock.writeLock();
+        wLock.lock();
+        try {
+            list.add(element);
+        } finally {
+            wLock.unlock();
+        }
         // write your code here
     }
 
     public E get(int index) {
+        Lock lock1 = lock.readLock();
+        lock1.lock();
+        try {
+            return list.get(index);
+        } finally {
+            lock1.unlock();
+        }
         // write your code here
-        return null;
     }
 
     public int size() {
         // write your code here
-        return 0;
+        return list.size();
     }
 }
