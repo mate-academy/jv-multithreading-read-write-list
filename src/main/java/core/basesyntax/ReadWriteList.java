28065484147ff93cee044791b5861ccfe38f4371
Nataliia <gzodchie@gmail.com>
2024-01-24 16:16:23 +0200

    lock variables is moved on class level

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,37 +2,39 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
-    private List<E> list = new ArrayList<>();
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final List<E> list = new ArrayList<>();
+    private final ReentrantReadWriteLock.ReadLock readLock =
+            new ReentrantReadWriteLock().readLock();
+    private final ReentrantReadWriteLock.WriteLock writeLock =
+            new ReentrantReadWriteLock().writeLock();
 
     public void add(E element) {
-        lock.writeLock().lock();
+        writeLock.lock();
         try {
             list.add(element);
         } finally {
-            lock.writeLock().unlock();
+            writeLock.unlock();
         }
     }
 
     public E get(int index) {
-        lock.readLock().lock();
+        readLock.lock();
         try {
             return list.get(index);
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
     }
 
     public int size() {
-        lock.readLock().lock();
+        readLock.lock();
         try {
             return list.size();
         } finally {
-            lock.readLock().unlock(); 
+            readLock.unlock();
         }
     }
 }
