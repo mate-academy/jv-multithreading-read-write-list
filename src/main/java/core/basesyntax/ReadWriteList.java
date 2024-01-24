cfda1595e1798435e76b576f56cca12969a11bf1
Danil Ponomarenko <akame.dante13@gmail.com>
2024-01-24 12:02:23 +0200

    Optimized `size()` method & introduce writeLock and readLock as a class level variables

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,33 +2,41 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
     private List<E> list = new ArrayList<>();
     private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock writeLock = lock.writeLock();
+    private final Lock readLock = lock.readLock();
 
     public void add(E element) {
-        lock.writeLock().lock();
+        writeLock.lock();
         try {
             list.add(element);
         } finally {
-            lock.writeLock().lock();
+            writeLock.lock();
         }
     }
 
     public E get(int index) {
-        lock.readLock().lock();
+        readLock.lock();
         try {
             list.get(index);
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
         return null;
     }
 
     public int size() {
-        return list.size();
+        readLock.lock();
+        try {
+            return list.size();
+        } finally {
+            readLock.unlock();
+        }
     }
 }
