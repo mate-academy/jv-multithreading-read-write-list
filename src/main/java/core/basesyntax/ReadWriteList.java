d7b407359417938dd32d3281ba2d596ddb55365c
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-01-29 21:55:10 +0200

    implemnted three methods - add, get and size

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -7,19 +7,34 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
     private List<E> list = new ArrayList<>();
+    private int size;
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
+    public ReadWriteList() {
+        this.list = new ArrayList<>();
+        size = 0;
+    }
+
     public void add(E element) {
-        // write your code here
+        lock.writeLock().lock();
+        try {
+            list.add(element);
+            size++;
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        lock.readLock().lock();
+        try {
+            return list.get(index);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        return size;
     }
 }
