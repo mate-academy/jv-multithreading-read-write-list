44093ff08045ebff648f4a96bb90a76c8e0088c2
Roman Globa <globaroman@gmail.com>
2024-03-08 19:01:13 +0200

    The solving task+

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -30,6 +30,11 @@ public class ReadWriteList<E> {
     }
 
     public int size() {
-        return list.size();
+        lock.readLock().lock();
+        try {
+            return list.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
