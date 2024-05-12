b39ec7d5e01a9bffed4749c34012d2e6ac3dbd73
ilko-ilya <samilyakilya@gmail.com>
2024-05-12 18:07:31 +0300

    first commit

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -10,16 +10,29 @@ public class ReadWriteList<E> {
     private ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        // write your code here
+        lock.readLock().lock();
+        try {
+            list.add(element);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        lock.writeLock().lock();
+        try {
+            return list.get(index);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        lock.readLock().lock();
+        try {
+            return list.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
