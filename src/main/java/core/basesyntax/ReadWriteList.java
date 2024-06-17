1732ce0d5f9914ddb83a95ac4ad03af6b6ad1b30
HAIOVIY <gaevoywork@gmail.com>
2024-06-17 14:19:21 +0300

    add read and write lock fields

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -2,41 +2,40 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
     private List<E> list = new ArrayList<>();
     private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private Lock readLock = lock.readLock();
+    private Lock writeLock = lock.writeLock();
 
     public void add(E element) {
-        lock.writeLock().lock();
+        writeLock.lock();
         try {
             list.add(element);
         } finally {
-            lock.writeLock().unlock();
+            writeLock.unlock();
         }
     }
 
     public E get(int index) {
-        lock.readLock().lock();
+        readLock.lock();
         try {
-            if (index < 0 || index >= list.size()) {
-                throw new RuntimeException(
-                        "Index " + index + " out of bounds from size: " + list.size());
-            }
             return list.get(index);
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
     }
 
     public int size() {
-        lock.readLock().lock();
+        readLock.lock();
         try {
             return list.size();
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
     }
 }
