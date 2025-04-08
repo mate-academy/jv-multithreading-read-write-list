197a863c4d403a1c966c2573c03cbde35d457898
Oleh <popovich158@gmail.com>
2025-04-08 23:30:14 +0300

    done

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -6,20 +6,33 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteList<E> {
-    private List<E> list = new ArrayList<>();
-    private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final List<E> list = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public void add(E element) {
-        // write your code here
+        lock.writeLock().lock(); // блокування для запису
+        try {
+            list.add(element);
+        } finally {
+            lock.writeLock().unlock(); // розблокування
+        }
     }
 
     public E get(int index) {
-        // write your code here
-        return null;
+        lock.readLock().lock(); // блокування для читання
+        try {
+            return list.get(index);
+        } finally {
+            lock.readLock().unlock(); // розблокування
+        }
     }
 
     public int size() {
-        // write your code here
-        return 0;
+        lock.readLock().lock(); // блокування для читання
+        try {
+            return list.size();
+        } finally {
+            lock.readLock().unlock(); // розблокування
+        }
     }
 }
