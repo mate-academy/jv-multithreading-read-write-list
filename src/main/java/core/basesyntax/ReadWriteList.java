9c13c09e2f649644bef59149e16dac8a420e6361
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-04-15 17:10:04 +0200

    First solution

--- src/main/java/core/basesyntax/ReadWriteList.java
+++ src/main/java/core/basesyntax/ReadWriteList.java
@@ -11,15 +11,28 @@ public class ReadWriteList<E> {
 
     public void add(E element) {
         // write your code here
+        lock.writeLock().lock();
+        try {
+            list.add(element);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public E get(int index) {
         // write your code here
-        return null;
+        lock.readLock().lock();
+        E element;
+        try {
+            element = list.get(index);
+        } finally {
+            lock.readLock().unlock();
+        }
+        return element;
     }
 
     public int size() {
         // write your code here
-        return 0;
+        return list.size();
     }
 }
